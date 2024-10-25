1ca3d708900e03386a4c8fcbfe58c413d2a35964
leejungyan <119379713+leejungyan08@users.noreply.github.com>
2024-10-25 09:11:51 +0800

    update

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,10 +1,16 @@
-import React, { useRef, useState, useContext, useEffect, useCallback } from "react";
+import React, {
+  useRef,
+  useState,
+  useContext,
+  useEffect,
+  useCallback,
+} from "react";
 import DishIcon from "../assets/icons/dish.svg";
 import Counter from "../components/Counter";
 import BasketBtn from "../components/BasketBtn";
 import { ProductContext } from "../context/ProductContext";
 import { ArrowLeftDoubleIcon, ArrowRightDoubleIcon } from "hugeicons-react";
-import '../css/menu.css'
+import "../css/menu.css";
 interface Product {
   ID: number;
   NAME: string;
@@ -15,6 +21,7 @@ interface Product {
 const Menu: React.FC = () => {
   const [leftIconShow, setLeftIconShow] = useState<boolean>(false);
   const [rightIconShow, setRightIconShow] = useState<boolean>(false);
+  const [selectedCat, setSelectedCat] = useState<string>("All");
   const tabBox = useRef<HTMLDivElement>(null);
   const productContext = useContext(ProductContext);
   useEffect(() => {
@@ -43,20 +50,18 @@ const Menu: React.FC = () => {
 
   const slideLeft: () => void = () => {
     tabBox.current!.scrollLeft -= 300;
-   
   };
-  const tabBoxScroll: () => void = () =>{
+  const tabBoxScroll: () => void = () => {
     handleIcons();
-  }
+  };
 
- 
   const handleIcons: () => void = () => {
     const tempScrollValue: number = tabBox.current!.scrollLeft;
-    const scrollValue: number = Math.round(tempScrollValue)+1;
+    const scrollValue: number = Math.round(tempScrollValue) + 1;
     const maxScrollWidth: number =
       tabBox.current!.scrollWidth - tabBox.current!.clientWidth;
-    console.log({scrollValue,maxScrollWidth});
-    
+    console.log({ scrollValue, maxScrollWidth });
+
     if (scrollValue > 1) {
       setLeftIconShow(true);
     } else {
@@ -69,13 +74,13 @@ const Menu: React.FC = () => {
       setRightIconShow(true);
     }
   };
- 
+
   return (
     <div className="flex-1 flex flex-col py-5 ">
       <span className="font-nunito font-black text-gray-700 uppercase text-3xl px-3">
         menu
       </span>
-   
+
       <div id="cat" className="flex mt-2 w-full items-center  relative">
         <div className={`icon ${leftIconShow ? "flex" : "hidden"}`}>
           <div
@@ -91,32 +96,42 @@ const Menu: React.FC = () => {
           className="flex items-center  gap-2 px-3 py-3"
           onScroll={tabBoxScroll}
         >
-          <div
-            className="capitalize font-poppins font-semibold w-fit shrink-0
-                                 text-sm rounded-full  px-4 py-2 bg-primaryColor text-white cursor-pointer shadow-md shadow-primaryColor flex gap-2"
+          <button
+            onClick={() => setSelectedCat("All")}
+            className={`capitalize font-poppins font-semibold w-fit shrink-0 
+                                 text-sm rounded-full  px-4 py-2  cursor-pointer  flex gap-2 ${
+                                   selectedCat == "All"
+                                     ? "bg-primaryColor text-white shadow-md shadow-primaryColor"
+                                     : "bg-gray-200 text-gray-700 hover:bg-gray-300 shadow-sm"
+                                 }`}
           >
             <img
               src={DishIcon}
               className="w-4 h-4 object-contain flex-1 shrink-0"
             ></img>
             <span className="flex-1 shrink-0">All</span>
-          </div>
+          </button>
           {uniqueTypes &&
             uniqueTypes.map((type, index) => {
               return (
-                <div
+                <button
                   key={index}
-                  className={`
+                  className={`  ${
+                    selectedCat == type
+                      ? "bg-primaryColor text-white shadow-md shadow-primaryColor"
+                      : "bg-gray-200 text-gray-700 hover:bg-gray-300 shadow-sm"
+                  }
                                 capitalize font-poppins font-semibold shrink-0
                                  text-sm rounded-full px-4 py-2 
-                                cursor-pointer  bg-gray-200 text-gray-700 hover:bg-gray-300  transition-all flex gap-2`}
+                                cursor-pointer   transition-all flex gap-2`}
+                  onClick={() => setSelectedCat(type)}
                 >
                   <img
                     src={getImageUrl(type) ? getImageUrl(type) : ""}
                     className="w-4 h-4 object-contain"
                   ></img>
                   <span>{type}</span>
-                </div>
+                </button>
               );
             })}
         </div>
